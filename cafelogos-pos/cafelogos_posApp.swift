48d2d6925c3cf08ee84516409785adc6db636106
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-08-08 21:18:33 +0900

    Initial Commit

---
+++ cafelogos-pos/cafelogos_posApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafelogos_posApp.swift
+//  cafelogos-pos
+//
+//  Created by ygates on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct cafelogos_posApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
