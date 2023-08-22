5e33851c884afdc47ef9d0a0770a46a11ce786b7
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-08-23 02:57:31 +0900

    feat: HomeMainButtonにNavigationLinkを追加

--- cafelogos-pos/Presentation/HomeView/Components/HomeMainButton.swift
+++ cafelogos-pos/Presentation/HomeView/Components/HomeMainButton.swift
@@ -7,36 +7,41 @@
 
 import SwiftUI
 
-struct HomeMainButton: View {
+struct HomeMainButton<Content: View>: View {
     var title: String
     var subtitle: String
     var description: String
+    var destination: Content
     
     var body: some View {
-        VStack(spacing: 0) {
-            Text(title)
-                .font(.system(.largeTitle, weight: .semibold))
-                .lineLimit(0)
-            Text(subtitle)
-                .font(.title3)
-                .lineLimit(2)
-            Text(description)
-                .font(.body)
-                .multilineTextAlignment(.center)
-                .lineLimit(2)
-                .padding(.top, 10)
-        }
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .aspectRatio(1/1, contentMode: .fit)
-        .clipped()
-        .padding(.horizontal, 30)
-        .padding(.vertical, 30)
-        .background {
-            RoundedRectangle(cornerRadius: 8, style: .continuous)
-                .fill(Color(.systemFill))
+        NavigationLink(destination: destination) {
+            VStack(spacing: 0) {
+                Text(title)
+                    .font(.system(.largeTitle, weight: .semibold))
+                    .lineLimit(0)
+                Text(subtitle)
+                    .font(.title3)
+                    .lineLimit(2)
+                Text(description)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .padding(.top, 10)
+            }
+            .foregroundColor(.primary)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+            .aspectRatio(1/1, contentMode: .fit)
+            .clipped()
+            .padding(.horizontal, 30)
+            .padding(.vertical, 30)
+            .background {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(Color(.systemFill))
+            }
         }
     }
 }
+
 
 struct HomeMainButton_Previews: PreviewProvider {
     static var previews: some View {
@@ -44,10 +49,13 @@ struct HomeMainButton_Previews: PreviewProvider {
         @State var displayConnection: Bool = true
         NavBarBody(displayConnection: $displayConnection, serverConnection: $serverConnection, title: "ホーム", content: {
             Divider()
-            HomeMainButton(title: "点検", subtitle: "アツアツ", description: "")
+            HomeMainButton(title: "Title", subtitle: "Subtitle", description: "Description", destination:
+                            OrderInputView()
+            )
+            
             
         })
-            .previewInterfaceOrientation(.landscapeLeft)
-            .previewDevice("iPad Pro (11-inch) (4th generation)")
+        .previewInterfaceOrientation(.landscapeLeft)
+        .previewDevice("iPad Pro (11-inch) (4th generation)")
     }
 }
