1bee83044dae318a7fa5112fe0753556d6250002
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-08-23 02:01:02 +0900

    feat: HomeMainButtonを実装

---
+++ cafelogos-pos/Presentation/HomeView/Components/HomeMainButton.swift
@@ -0,0 +1,20 @@
+//
+//  HomeMainButton.swift
+//  cafelogos-pos
+//
+//  Created by Owner on 2023/08/23.
+//
+
+import SwiftUI
+
+struct HomeMainButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeMainButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeMainButton()
+    }
+}
