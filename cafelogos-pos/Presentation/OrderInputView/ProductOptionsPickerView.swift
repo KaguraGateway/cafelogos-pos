5b121dcc22f46a625295dfe4d0e62941f582955f
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-09-20 16:51:55 +0900

    feat: ProductOptionsPickerViewを作成

---
+++ cafelogos-pos/Presentation/OrderInputView/ProductOptionsPickerView.swift
@@ -0,0 +1,22 @@
+//
+//  ProductOptionsPickerView.swift
+//  cafelogos-pos
+//
+//  Created by Owner on 2023/09/20.
+//
+
+import SwiftUI
+
+struct ProductOptionsPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductOptionsPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductOptionsPickerView()
+            .previewInterfaceOrientation(.landscapeRight)
+            .previewDevice("iPad Pro (11-inch) (4th generation)")
+    }
+}
