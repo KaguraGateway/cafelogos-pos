3ca5ba7bb1d75e309a45a64984f3fdc73defd712
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-09-29 17:49:27 +0900

    コメントアウトを削除

---
+++ cafelogos-pos/Presentation/Transactions/InspectionView.swift
@@ -0,0 +1,18 @@
+//
+//  InspectionView.swift
+//  cafelogos-pos
+//
+//  Created by Owner on 2023/09/29.
+//
+
+import SwiftUI
+
+struct InspectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InspectionView()
+}
--- cafelogos-pos/Presentation/Transactions/StartTransactionView.swift
+++ cafelogos-pos/Presentation/Transactions/StartTransactionView.swift
@@ -43,39 +43,6 @@ struct StartTransactionView: View {
         }
     }
 }
-//struct multi1Button<Destination>: View where Destination : View {
-//    var title: String
-//    var bgColor: Color
-//    var fgColor: Color
-//    var destination: () -> Destination
-//    
-//    public init(title: String, bgColor: Color, fgColor: Color, destination: @escaping () -> Destination) {
-//        self.title = title
-//        self.bgColor = bgColor
-//        self.fgColor = fgColor
-//        self.destination = destination
-//    }
-//    
-//    var body: some View {
-//        NavigationLink(destination: destination) {
-//            VStack(spacing: 0) {
-//                Text(title)
-//                    .font(.largeTitle)
-//                    .fontWeight(.semibold)
-//                    .lineLimit(0)
-//            }
-//            .foregroundColor(fgColor)
-//            .frame(maxWidth: .infinity, minHeight: 50, alignment: .center)
-//            .clipped()
-//            .padding(.vertical, 20)
-//            .background {
-//                RoundedRectangle(cornerRadius: 8, style: .continuous)
-//                    .fill(bgColor)
-//            }
-//            .padding(.bottom)
-//        }
-//    }
-//}
 
 struct multi2Button<Destination>: View where Destination : View {
     var title: String
