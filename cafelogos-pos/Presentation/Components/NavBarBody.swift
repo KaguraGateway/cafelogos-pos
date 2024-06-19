58bcc287733686782d0bfdde71f4dd866dab3662
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-06-19 17:31:23 +0900

    fix: 料金モニター表示を削除

--- cafelogos-pos/Presentation/Components/NavBarBody.swift
+++ cafelogos-pos/Presentation/Components/NavBarBody.swift
@@ -36,12 +36,12 @@ struct NavBarBody <Content: View>: View {
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
                         HStack(spacing: 10) {
-                            VStack(alignment: .center) {
-                                Image(systemName: displayConnection ? "checkmark.circle.fill" : "xmark.circle.fill")
-                                    .foregroundColor(displayConnection ? .green : .red)
-                                Text("料金モニター")
-                                    .foregroundColor(displayConnection ? .green : .red)
-                            }
+//                            VStack(alignment: .center) {
+//                                Image(systemName: displayConnection ? "checkmark.circle.fill" : "xmark.circle.fill")
+//                                    .foregroundColor(displayConnection ? .green : .red)
+//                                Text("料金モニター")
+//                                    .foregroundColor(displayConnection ? .green : .red)
+//                            }
                             
                             VStack {
                                 Image(systemName: serverConnection ? "checkmark.circle.fill" : "xmark.circle.fill")
