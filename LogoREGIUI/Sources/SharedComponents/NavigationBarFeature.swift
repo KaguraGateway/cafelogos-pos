f09cb121a999e3bdf0300cd93e6d944f494cd33f
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-02 12:01:47 +0900

    feat: ビルドが通るNavigationBarFeatureを実装

--- LogoREGIUI/Sources/SharedComponents/NavigationBarFeature.swift
+++ LogoREGIUI/Sources/SharedComponents/NavigationBarFeature.swift
@@ -7,3 +7,47 @@
 
 import Foundation
 import ComposableArchitecture
+
+@Reducer
+struct NavigationBarFeature {
+    @ObservableState
+    struct State {
+        var displayConnection: Bool = true
+        var serverConnection: Bool = true
+        var title: String = ""
+    }
+
+    enum Action {
+            case onAppear
+            case toggleDisplayConnection
+            case toggleServerConnection
+            case setTitle(String)
+            case openDrawer
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .none
+                
+            case .toggleDisplayConnection:
+                state.displayConnection.toggle()
+                return .none
+                
+            case .toggleServerConnection:
+                state.serverConnection.toggle()
+                return .none
+                
+            case let .setTitle(newTitle):
+                state.title = newTitle
+                return .none
+                
+            case .openDrawer:
+                // ドロワー操作は別のFeatureに切り分けるべきという判断で実装見送り
+                print("openDrawer")
+                return .none
+            }
+        }
+    }
+}
