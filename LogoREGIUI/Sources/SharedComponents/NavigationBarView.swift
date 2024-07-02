2c856db4f524749f7c17ad5087f9da8edebe658f
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-02 12:42:08 +0900

    feat: NavigationBarViewを実装

--- LogoREGIUI/Sources/SharedComponents/NavigationBarView.swift
+++ LogoREGIUI/Sources/SharedComponents/NavigationBarView.swift
@@ -1,18 +1,48 @@
 //
-//  SwiftUIView.swift
-//  
+//  NavigationBarView.swift
+//
 //
 //  Created by Owner on 2024/07/02.
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct NavigationBarView: View {
+    @Bindable var store: StoreOf<NavigationBarFeature>
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // ここにコンテンツを配置
+            }
+            .background(Color(.secondarySystemBackground))
+            .navigationTitle(store.title)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 10) {
+                        VStack {
+                            Image(systemName: store.serverConnection ? "checkmark.circle.fill" : "xmark.circle.fill")
+                                .foregroundColor(store.serverConnection ? .green : .red)
+                            Text("サーバー通信")
+                                .foregroundColor(store.serverConnection ? .green : .red)
+                        }
+                        Button("ドロアを開く") {
+                            store.send(.openDrawer)
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear { store.send(.onAppear) }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    NavigationBarView(
+        store: Store(initialState: NavigationBarFeature.State(title: "ホーム")) {
+            NavigationBarFeature()
+        }
+    )
 }
