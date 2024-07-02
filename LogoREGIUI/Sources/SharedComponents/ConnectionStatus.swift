692045aa75847362db074aa7e863a32bd3800329
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-02 17:25:33 +0900

    fix: インデントを修正

--- LogoREGIUI/Sources/SharedComponents/ConnectionStatus.swift
+++ LogoREGIUI/Sources/SharedComponents/ConnectionStatus.swift
@@ -13,11 +13,11 @@ struct ConnectionStatus: View {
     
     var body: some View {
         VStack {
-                    Image(systemName: connectionStatus ? "checkmark.circle.fill" : "xmark.circle.fill")
-                        .foregroundColor(connectionStatus ? .green : .red)
-                    Text(connectionName)
-                        .foregroundColor(connectionStatus ? .green : .red)
-                }
+            Image(systemName: connectionStatus ? "checkmark.circle.fill" : "xmark.circle.fill")
+                .foregroundColor(connectionStatus ? .green : .red)
+            Text(connectionName)
+                .foregroundColor(connectionStatus ? .green : .red)
+        }
     }
 }
 
