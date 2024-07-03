88d036bac843bc7fa02f694c4fa9810ee75552a0
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-04 07:14:19 +0900

    viewModelをStoreにリプレイス

--- LogoREGIUI/Sources/Features/OrderEntryFeature/OrderEntryView.swift
+++ LogoREGIUI/Sources/Features/OrderEntryFeature/OrderEntryView.swift
@@ -14,7 +14,7 @@ import ComposableArchitecture
 struct OrderEntryView: View {
     @Bindable var store: StoreOf<OrderEntryFeature>
     
-    @ObservedObject private var viewModel = OrderEntryViewModel()
+    //    @ObservedObject private var viewModel = OrderEntryViewModel()
     
     var body: some View {
         GeometryReader{geometry in
@@ -22,51 +22,55 @@ struct OrderEntryView: View {
                 VStack(spacing: 0){
                     HStack(spacing:0){
                         ProductStack(
-                            productCategories: viewModel.categoriesWithProduct,
-                            onAddItem: viewModel.addItem
+                            productCategories: store.categoriesWithProduct,
+                            onAddItem: { product, brew in
+                                store.send(.addItem(product, brew))
+                            }
                         )
                         .padding(.leading, 10)
                         .frame(width: geometry.size.width * 0.6)
                         Divider()
-                        DiscountStack(discounts: viewModel.discounts, onTapDiscount: viewModel.onTapDiscount)
+                        DiscountStack(
+                            discounts: store.discounts,
+                            onTapDiscount: {discount in
+                                store.send(.onTapDiscount(discount))
+                            }
+                            )
                             .frame(width: geometry.size.width * 0.1)
                         Divider()
                         OrderEntryStack(
-                            cartItems: viewModel.order.cart.items,
-                            discounts: viewModel.order.discounts,
-                            onTapDecreaseBtn: viewModel.onTapDecrease,
-                            onTapIncreaseBtn: viewModel.onTapIncrease,
-                            onRemoveItem: viewModel.onRemoveItem
+                            cartItems: store.order.cart.items,
+                            discounts: store.order.discounts,
+                            onTapDecreaseBtn: { item in
+                                store.send(.onTapDecrease(item))
+                            },
+                            onTapIncreaseBtn: { item in
+                                store.send(.onTapDecrease(item))
+                            },
+                            onRemoveItem: { item in
+                                store.send(.onRemoveItem(item))
+                            }
                         )
                         .frame(width: geometry.size.width * 0.3)
                     }
                     EntryBottomBar(
-                        order: viewModel.order, onRemoveAllItem: viewModel.onRemoveAllItem
+                        order: store.order, 
+                        onRemoveAllItem: {
+                            store.send(.onRemoveAllItem)
+                        }
                     )
                     
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color(.secondarySystemBackground))
                 .navigationBarBackButtonHidden(true)
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        NavigationLink{
-                            HomeView()
-                        } label:{
-                            Text("ホームへ戻る")
-                        }
-                    }
-                }
             }
         }
-        .task {
-            await self.viewModel.fetch()
+        .onAppear {
+            store.send(.onAppear)
         }
     }
 }
-
-
-// ここから注文リストのボタン
 
 
 
