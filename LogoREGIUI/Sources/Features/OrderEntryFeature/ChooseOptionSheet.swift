daba8cc52ef294bc067a154a80ddb1ff13d6a759
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-05 00:05:31 +0900

    ChooseOptionViewをファイル分割

--- LogoREGIUI/Sources/Features/OrderEntryFeature/ChooseOptionSheet.swift
+++ LogoREGIUI/Sources/Features/OrderEntryFeature/ChooseOptionSheet.swift
@@ -1,26 +1,6 @@
 import SwiftUI
 import LogoREGICore
 
-struct ChooseOptionSheetView: View {
-    @Binding var selectProduct: ProductDto?
-    var onTapCoffeeBrew: (ProductDto, Option) -> Void
-    
-    func getOptions(product: ProductDto) -> [Option] {
-        if(product.coffeeHowToBrews == nil) {
-            return [Option]()
-        }
-        return product.coffeeHowToBrews!.map {
-            Option(id: $0.id, title: $0.name, description: "¥\($0.amount)")
-        }
-    }
-    
-    var body: some View {
-        ChooseOptionSheet(productName: "ドリップ方法", options: getOptions(product: selectProduct!), onAction: { option in
-            onTapCoffeeBrew(selectProduct!, option)
-        })
-    }
-}
-
 struct ChooseOptionSheet: View {
     @Environment(\.dismiss) var dismiss
     public let productName: String
