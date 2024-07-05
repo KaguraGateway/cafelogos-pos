9f6fb144645b11eda93318ea22a9a44429eb0de6
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-05 20:27:24 +0900

    DenominationFormをTCA化

--- LogoREGIUI/Sources/Features/CashDrawerOperationsFeature/DenominationFormsFeature/DenominationForm.swift
+++ LogoREGIUI/Sources/Features/CashDrawerOperationsFeature/DenominationFormsFeature/DenominationForm.swift
@@ -3,7 +3,8 @@ import SwiftUI
 import LogoREGICore
 
 struct DenominationForm: View {
-    @Binding var denomination: Denomination
+    let denomination: Denomination
+    let onUpdate: (Denomination) -> Void
     
     var body: some View {
         HStack(alignment: .center){
@@ -19,35 +20,42 @@ struct DenominationForm: View {
             Spacer()
             Text("×")
                 .padding(.trailing)
-            TextField("0", value: Binding(get: { denomination.quantity }, set: { denomination.setQuantity(newValue: $0) }), formatter: NumberFormatter())
-                .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidBeginEditingNotification), perform: { obj in
-                    if let textField = obj.object as? UITextField {
-                        textField.selectAll(textField.text)
+            TextField("0", value: Binding(
+                get: { denomination.quantity },
+                set: { newValue in
+                    var updatedDenomination = denomination
+                    updatedDenomination.setQuantity(newValue: newValue)
+                    onUpdate(updatedDenomination)
+                }
+                    ), formatter: NumberFormatter())
+                        .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidBeginEditingNotification), perform: { obj in
+                            if let textField = obj.object as? UITextField {
+                                textField.selectAll(textField.text)
+                                
+                            }
+                        })
+                        .multilineTextAlignment(.trailing)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(maxWidth: 180)
+                    
+                    
+                    Text("=")
+                    HStack(){
+                        Spacer()
+                        Text("¥\(denomination.total())")
+                        
                         
                     }
-                })
-                .multilineTextAlignment(.trailing)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .frame(maxWidth: 180)
-            
-            
-            Text("=")
-            HStack(){
-                Spacer()
-                Text("¥\(denomination.total())")
+                    .frame(maxWidth: 100)
+                    .padding(.trailing)
+                }
+                    .frame(maxWidth: .infinity)
                 
                 
-            }
-            .frame(maxWidth: 100)
-            .padding(.trailing)
-        }
-        .frame(maxWidth: .infinity)
-        
-        
-        
-        .padding(.vertical, 5)
-        
-        .font(.title3)
-        
-    }
-}
+                
+                    .padding(.vertical, 5)
+                
+                    .font(.title3)
+                
+                }
+                }
