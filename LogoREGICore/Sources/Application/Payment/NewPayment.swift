a16b7c6d2af32068018bf73e3eab8b26a83877bb
Yukinaga OISHI <c1223781@st.kanazawa-it.ac.jp>
2024-11-29 09:56:25 +0900

    fix: 必要のないメソッドを削除

--- LogoREGICore/Sources/Application/Payment/NewPayment.swift
+++ LogoREGICore/Sources/Application/Payment/NewPayment.swift
@@ -20,38 +20,6 @@ public struct NewPayment {
     public func Execute(payment: Payment, postOrder: Order?) async -> NewPaymentOutput {
         let config = configRepo.load()
 
-        var orderSummary = ""
-         商品リストを取得
-        if let order = postOrder {
-            for cartItem in order.cart.items {
-                // 商品名
-                let productName = cartItem.productName
-                // 個数
-                let quantity = cartItem.quantity
-                // 淹れ方
-                if let coffeeHowToBrew = cartItem.coffeeHowToBrew {
-                    let brewMethod = coffeeHowToBrew.name // 淹れ方名
-                    let itemSummary = "・\(productName)(\(brewMethod)) x\(quantity) ¥\(cartItem.productPrice)"
-                    orderSummary += itemSummary + "\n"
-                } else {
-                    // coffeeHowToBrewがない場合
-                    if let otherProduct = cartItem.otherProduct {
-                        let otherProductName = otherProduct.productName
-                        let price = otherProduct.price
-                        let itemSummary = "・\(otherProductName) x\(quantity) ¥\(price)"
-                        orderSummary += itemSummary + "\n"  // 商品情報を追加
-                    } else {
-                        // otherProductもない場合
-                        let itemSummary = "・\(productName) x\(quantity) ¥\(cartItem.productPrice)"
-                        orderSummary += itemSummary + "\n"
-                    }
-                }
-            }
-        }
-        
-        print(orderSummary)  // 結果を表示
-         商品リストを生成
-
         var receiptItems: [ReceiptItem] = []
         if let order = postOrder {
             receiptItems = generateReceiptItems(order: order)
