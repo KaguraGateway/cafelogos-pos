2f1814d132156a640647deca687f679a4ba3fb2a
Yukinaga OISHI <c1223781@st.kanazawa-it.ac.jp>
2024-11-29 09:52:34 +0900

    feat: レシート情報を生成するメソッドを実装

--- LogoREGICore/Sources/Application/Payment/NewPayment.swift
+++ LogoREGICore/Sources/Application/Payment/NewPayment.swift
@@ -19,8 +19,9 @@ public struct NewPayment {
     
     public func Execute(payment: Payment, postOrder: Order?) async -> NewPaymentOutput {
         let config = configRepo.load()
+
         var orderSummary = ""
-        // 商品リストを取得
+         商品リストを取得
         if let order = postOrder {
             for cartItem in order.cart.items {
                 // 商品名
@@ -47,20 +48,55 @@ public struct NewPayment {
                 }
             }
         }
-
+        
         print(orderSummary)  // 結果を表示
+         商品リストを生成
 
+        var receiptItems: [ReceiptItem] = []
+        if let order = postOrder {
+            receiptItems = generateReceiptItems(order: order)
+        }
+        print(receiptItems)
+        
         let res = await paymentService.postPayment(payment: payment, postOrder: postOrder)
         if res.error == nil {
             paymentRepo.save(payment: payment)
             
             if(config.isUsePrinter) {
                 await cashierAdapter.openCacher()
-                await cashierAdapter.printReceipt(receipt: OrderReceipt(callNumber: "\(res.callNumber)\n\(orderSummary)" ?? ""))
+                await cashierAdapter.printReceipt(receipt: OrderReceipt(callNumber: res.callNumber ?? ""))
             }
         }
         return NewPaymentOutput(callNumber: res.callNumber ?? "", error: res.error)
     }
+    
+    private func generateReceiptItems(order: Order) -> [ReceiptItem] {
+        var items: [ReceiptItem] = []
+        
+        for cartItem in order.cart.items {
+            let productName = cartItem.productName // 商品名
+            let quantity = cartItem.quantity // 個数
+            var brewMethod: String? = nil
+            let price: Int = Int(cartItem.productPrice)
+            
+            if let coffeeHowToBrew = cartItem.coffeeHowToBrew {
+                // 淹れ方がある場合
+                brewMethod = coffeeHowToBrew.name
+            }
+            // TODO: OtherProductまわりの処理が必要だったら追加する
+            
+            let receiptItem = ReceiptItem(
+                name: productName,
+                brewMethod: brewMethod,
+                quantity: Int(quantity),
+                price: price
+            )
+            items.append(receiptItem)
+        }
+        
+        return items
+    }
+
 }
 
 public struct NewPaymentOutput {
