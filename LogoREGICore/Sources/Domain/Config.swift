faecf23093587d8a9aea26fed581d659135aa05f
Yukinaga OISHI <c1223781@st.kanazawa-it.ac.jp>
2025-01-16 00:46:44 +0900

    swift format

--- LogoREGICore/Sources/Domain/Config.swift
+++ LogoREGICore/Sources/Domain/Config.swift
@@ -10,43 +10,43 @@ import ULID
 
 
 public struct Config: Equatable {
-public let clientId: String
-public var clientName: String
-public var isTrainingMode: Bool
-public var isUsePrinter: Bool
-public var isPrintKitchenReceipt: Bool
-public var hostUrl: String
-
-public var isUseSquareTerminal: Bool
-public var squareAccessToken: String
-public var squareTerminalDeviceId: String
-
-public init() {
-    self.clientId = ULID().ulidString
-    self.clientName = ""
-    self.isTrainingMode = false
-    self.isUsePrinter = false
-    self.isPrintKitchenReceipt = false
-    self.hostUrl = "http://localhost:8080"
-    self.isUseSquareTerminal = false
-    self.squareAccessToken = ""
-    self.squareTerminalDeviceId = ""
-}
-
-public init(clientId: String, clientName: String, isTrainingMode: Bool, isUsePrinter: Bool, isPrintKitchenReceipt: Bool, isUseSquareTerminal: Bool, squareAccessToken: String, squareTerminalDeviceId: String, hostUrl: String) {
-    self.clientId = clientId
-    self.clientName = clientName
-    self.isTrainingMode = isTrainingMode
-    self.isUsePrinter = isUsePrinter
-    self.isPrintKitchenReceipt = isPrintKitchenReceipt
-    self.hostUrl = hostUrl
-    self.isUseSquareTerminal = isUseSquareTerminal
-    self.squareAccessToken = squareAccessToken
-    self.squareTerminalDeviceId = squareTerminalDeviceId
-}
+    public let clientId: String
+    public var clientName: String
+    public var isTrainingMode: Bool
+    public var isUsePrinter: Bool
+    public var isPrintKitchenReceipt: Bool
+    public var hostUrl: String
+    
+    public var isUseSquareTerminal: Bool
+    public var squareAccessToken: String
+    public var squareTerminalDeviceId: String
+    
+    public init() {
+        self.clientId = ULID().ulidString
+        self.clientName = ""
+        self.isTrainingMode = false
+        self.isUsePrinter = false
+        self.isPrintKitchenReceipt = false
+        self.hostUrl = "http://localhost:8080"
+        self.isUseSquareTerminal = false
+        self.squareAccessToken = ""
+        self.squareTerminalDeviceId = ""
+    }
+    
+    public init(clientId: String, clientName: String, isTrainingMode: Bool, isUsePrinter: Bool, isPrintKitchenReceipt: Bool, isUseSquareTerminal: Bool, squareAccessToken: String, squareTerminalDeviceId: String, hostUrl: String) {
+        self.clientId = clientId
+        self.clientName = clientName
+        self.isTrainingMode = isTrainingMode
+        self.isUsePrinter = isUsePrinter
+        self.isPrintKitchenReceipt = isPrintKitchenReceipt
+        self.hostUrl = hostUrl
+        self.isUseSquareTerminal = isUseSquareTerminal
+        self.squareAccessToken = squareAccessToken
+        self.squareTerminalDeviceId = squareTerminalDeviceId
+    }
 }
 
 protocol ConfigRepository {
-func load() -> Config
-func save(config: Config)
+    func load() -> Config
+    func save(config: Config)
 }
