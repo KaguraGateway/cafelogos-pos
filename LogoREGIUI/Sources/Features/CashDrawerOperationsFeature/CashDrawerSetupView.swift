6da6b0a21657d186d8dde3b205d70037cd71a262
yukinaga.oishi.a <c1223781@st.kanazawa-it.ac.jp>
2025-03-12 21:43:02 +0900

    feat: add screenshot function

--- LogoREGIUI/Sources/Features/CashDrawerOperationsFeature/CashDrawerSetupView.swift
+++ LogoREGIUI/Sources/Features/CashDrawerOperationsFeature/CashDrawerSetupView.swift
@@ -24,6 +24,12 @@ struct CashDrawerSetupView: View {
 
                         TitleNavButton(title: "レジ開け完了", bgColor: Color.cyan, fgColor: Color.white)
                             .simultaneousGesture(TapGesture().onEnded {
+                                // 描画しているUIViewを取得して渡してスクリーンショットを撮る
+                                if let windowScene = UIApplication.shared.connectedScenes
+                                    .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene,
+                                   let rootView = windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController?.view {
+                                    store.send(.takeScreenshot(rootView))
+                                }
                                 store.send(.startCashierTransaction)
                             })
                     }
