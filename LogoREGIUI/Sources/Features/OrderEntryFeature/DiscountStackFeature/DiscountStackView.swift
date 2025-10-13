3d28bb8d1e0ce02bd3ca8b9c42dae7e79ba007a2
MasudaKeitoLB <keito.masuda2005@gmail.com>
2025-10-13 18:49:55 +0900

    不要な変更の削除とコード整形

--- LogoREGIUI/Sources/Features/OrderEntryFeature/DiscountStackFeature/DiscountStackView.swift
+++ LogoREGIUI/Sources/Features/OrderEntryFeature/DiscountStackFeature/DiscountStackView.swift
@@ -1,13 +1,13 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Owner on 2024/07/04.
 //
 
-import SwiftUI
-import LogoREGICore
 import ComposableArchitecture
+import LogoREGICore
+import SwiftUI
 
 // 割引を表示するView
 struct DiscountStackView: View {
@@ -19,42 +19,53 @@ struct DiscountStackView: View {
                 // DiscountCell
                 LazyVGrid(columns: [GridItem(.flexible())]) {
                     ForEach(store.discounts, id: \.id) { discount in
-                        Button(action: {
-                            store.send(.onTapDiscount(discount))
-                        }, label: {
-                        VStack(spacing: 0) {
-                            // DiscountName
-                            Text(discount.name)
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                                .padding(.vertical, 5)
-                                .lineLimit(3)
-                                .multilineTextAlignment(.center)
-                            
-                            Spacer()
-                            
-                            // DiscountAmount
-                            Text("-¥\(discount.discountPrice)")
-                                .font(.title2)
-                                .fontWeight(.regular)
-                                .lineLimit(1)
-                        }
-                        .padding(10)
-                        .foregroundStyle(Color.primary)
-                        .frame(minHeight: 120)
-                        .frame(maxWidth: abs((geometry.size.width) - 12))
-                        .background {
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color(.tertiaryLabel), lineWidth: 1)
-                                .background(RoundedRectangle(cornerRadius: 8).fill(Color(.systemBackground)))
-                        }
-                        })
+                        Button(
+                            action: {
+                                store.send(.onTapDiscount(discount))
+                            },
+                            label: {
+                                VStack(spacing: 0) {
+                                    // DiscountName
+                                    Text(discount.name)
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
+                                        .padding(.vertical, 5)
+                                        .lineLimit(3)
+                                        .multilineTextAlignment(.center)
+
+                                    Spacer()
+
+                                    // DiscountAmount
+                                    Text("-¥\(discount.discountPrice)")
+                                        .font(.title2)
+                                        .fontWeight(.regular)
+                                        .lineLimit(1)
+                                }
+                                .padding(10)
+                                .foregroundStyle(Color.primary)
+                                .frame(minHeight: 120)
+                                .frame(
+                                    maxWidth: abs((geometry.size.width) - 12)
+                                )
+                                .background {
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(
+                                            Color(.tertiaryLabel),
+                                            lineWidth: 1
+                                        )
+                                        .background(
+                                            RoundedRectangle(cornerRadius: 8)
+                                                .fill(Color(.systemBackground))
+                                        )
+                                }
+                            }
+                        )
                     }
                 }
             }
             .padding(.top, 20)
         }
-        .onAppear{
+        .onAppear {
             store.send(.onAppear)
         }
     }
